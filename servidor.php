e61c8765cada0e776ae022d6542a139094867e46
jucasval <juanfrancastilla@gmail.com>
2020-10-09 09:12:33 +0200

    buSCAR

---
+++ servidor.php
@@ -0,0 +1,32 @@
+<?php 
+
+//Array de nombres
+    $personas = array(
+        "Sara", "Pepe", "María", "Juan", "Francisco", "Pedro", "Eva",
+        "Lola", "María del Carmen", "Inés", "Yolanda", "Esther", "Manuel", "Miguel",
+        "David", "José", "Hugo", "Luís"
+    );
+
+    $nombre = $_GET["nombre"];
+    $sugerencia = "";
+
+    if($nombre !== ""){
+        $nombre = strtolower($nombre);
+        $cantidadDeCaracteres = strlen($nombre);
+
+        foreach($personas as $persona){
+            $nombreEnServidor = substr($persona, 0, $cantidadDeCaracteres);
+
+            if(stristr($nombre, $nombreEnServidor) !== false){
+                if($sugerencia === ""){
+                    $sugerencia = $persona;
+                }else{
+                    $sugerencia .= ", $persona"
+                }
+            }
+        }
+
+        echo $sugerencia === "" ? "No fue encontrado" : $sugerencia;l
+    }
+
+?>
